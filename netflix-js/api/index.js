d201fbae3d6036916f6db05268e397f33fddcbce
Nubo <72999487+Nuboctane@users.noreply.github.com>
2022-04-01 15:01:14 +0200

    Add files via upload

--- netflix-js/api/index.js
+++ netflix-js/api/index.js
@@ -3,16 +3,26 @@ const app = express();
 const mongoose = require("mongoose");
 const dotevn = require("dotenv");
 const authRoute = require("./routes/auth");
-dotevn.config();
- 
-mongoose.connect(process.env.MONGO_URL)
-    .then(()=>console.log("DB Connection Succes"))
-    .catch(err=>console.log(err));
-  
-    app.use(express.json());
- 
-    app.use("/api/auth", authRoute);
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
 
-app.listen(8800, ()=>{
+dotevn.config();
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("DB Connection Succes"))
+    .catch(err => console.log(err));
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
+
+app.listen(8800, () => {
     console.log("Backen server is running");
 })
+
+
+
--- netflix-js/api/routes/auth.js
+++ netflix-js/api/routes/auth.js
@@ -1,45 +1,59 @@
 const router = require("express").Router();
-const { response } = require("express");
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async (req, res) => {
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: CryptoJS.AES.encrypt(
-            req.body.password,
-            process.env.SECRET_KEY
-        ).toString(),
-    });
-    console.log(newUser);
-    try {
-        const user = await newUser.save();
-        res.status(201).json(user);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString(),
+  });
+  try {
+    const user = await newUser.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //LOGIN
 router.post("/login", async (req, res) => {
-    try {
-        const user = await User.findOne({ email: req.body.email });
-        !user && res.status(401).json('Wrong password or username');
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+      const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+      const givenPassword = await req.body.password;
 
-        const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
-        const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+      console.log(user);
+      if (originalPassword !== givenPassword) {
+        console.log('11');
+        res.status(200).json({ error: "Wrong password or username!" });
+      } else {
+        console.log('22');
 
-        originalPassword !== req.body.password &&
-            res.status(401).json('Wrong password or username');
+        const accessToken = jwt.sign(
+          { id: user._id, isAdmin: user.isAdmin },
+          process.env.SECRET_KEY,
+          { expiresIn: "5d" }
+        );
 
         const { password, ...info } = user._doc;
 
-        res.status(200).json(user);
-    } catch (err) {
-        res.status(500).json(err);
+        res.status(200).json({ ...info, accessToken });
+      }
+    }else{
+      console.log('11');
+      res.status(200).json({ error: "Wrong password or username!" });
     }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
